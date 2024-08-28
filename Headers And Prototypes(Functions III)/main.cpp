9814fa0ae794cb15d53744d2cbf1a60b8b825d44
Michael Quaye Nii Ayi <millcode@outlook.com>
2024-08-28 22:10:44 +0000

    Add files via upload

---
+++ Headers And Prototypes(Functions III)/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+#include "quaye.h"
+
+using namespace std;
+
+
+
+int main()
+{
+    made();
+
+    return 0;
+}
+void made()
+{
+    cout << "\t                Headers and Prototype      "<<endl;
+    cout << endl;
+    cout << "\t      DETAILS  " <<endl;
+    cout << "Name:      Michael Quaye Nii Ayi" <<endl;
+    cout << "School:    Ghana Communication Technology University" <<endl;
+    cout << "Programme: BSc. Computer Science" <<endl;
+}
---
+++ Headers And Prototypes(Functions III)/quaye.h
@@ -0,0 +1,6 @@
+#ifndef QUAYE_H_INCLUDED
+#define QUAYE_H_INCLUDED
+
+void made();
+
+#endif // QUAYE_H_INCLUDED
