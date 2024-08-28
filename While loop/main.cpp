513e81dde4ab28755867c518ba63e27056f1be2d
Michael Quaye Nii Ayi <millcode@outlook.com>
2024-08-28 18:35:24 +0000

    Add files via upload

---
+++ While loop/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "\t ----WHILE LOOP CONDITION----" <<endl;
+    cout << "Code by: Michael N. A Quaye      Email: millcode@outlook.com" <<endl;
+    int a =0;
+    while(a <=9)
+    {
+        cout <<a << " Hello world!...I'm repeating 10 times" <<endl;
+        a++; //Increment a by 1
+    }
+    cout << "--------Repitition ends---------" <<endl;
+    return 0;
+}
