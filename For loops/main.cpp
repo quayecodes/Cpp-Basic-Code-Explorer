78abc6212c643e937b8340ea31ab545ef0ce19ca
Michael Quaye Nii Ayi <millcode@outlook.com>
2024-08-28 18:34:52 +0000

    Add files via upload

---
+++ For loops/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "\t --------------------------FOR LOOP----------------------" <<endl;
+    cout << "Code by: Michael N.A Quaye                     Email:millcode@outlook.com" <<endl;
+    cout <<endl;
+
+    for(int a=0; a <10; a++)
+    {
+      cout << "Hello! I love coding with C++......" <<endl;
+    }
+    return 0;
+}
